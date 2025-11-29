fa13ff8015e464eff073d2004a2a786812883d7c
NormanwOw <normjkeee@vk.com>
2025-11-29 13:07:40 +0500

    Init

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
+
+from fastapi import FastAPI
+
+from src.infrastructure.logger.impl import logger
+
+
+@asynccontextmanager
+async def lifespan(_application: FastAPI) -> AsyncIterator[None]:
+    logger.info("Start app...")
+    yield
+    logger.info("App shutdown")
+
+
+app = FastAPI(title="Order Service", version="0.0.1", lifespan=lifespan)
