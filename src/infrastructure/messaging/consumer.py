8f6367686f37e957e033f0129a35954638ba2294
NormanwOw <normjkeee@vk.com>
2026-01-17 11:08:06 +0500

    ADDED STOCKS_EVENTS_TOPIC into KafkaConsumer

--- src/infrastructure/messaging/consumer.py
+++ src/infrastructure/messaging/consumer.py
@@ -11,7 +11,7 @@ class KafkaConsumer(AIOKafkaConsumer):
             settings.SAGA_EVENTS_TOPIC,
             settings.ORDER_EVENTS_TOPIC,
             settings.PAYMENT_EVENTS_TOPIC,
-            settings.ANALYTIC_EVENTS_TOPIC,
+            settings.STOCKS_EVENTS_TOPIC,
             settings.NOTIFICATION_EVENTS_TOPIC,
             bootstrap_servers=settings.KAFKA_HOSTS,
             group_id='order_group',
