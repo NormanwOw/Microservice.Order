6aef0e676df284596ab89797f03b506de71b6e4e
NormanwOw <normjkeee@vk.com>
2026-01-17 11:07:41 +0500

    UPDATED CreateOrderSaga

--- src/application/sagas/create_order_saga.py
+++ src/application/sagas/create_order_saga.py
@@ -1,8 +1,14 @@
-from src.domain.enums import CreateOrderStepStatus, OrderStatus
+from src.domain.commands import SendNotifyCommand
+from src.domain.enums import CreateOrderSagaStatus, CreateOrderStepStatus, OrderEventTypes
+from src.domain.events import OrderPayed
 from src.infrastructure.exceptions import OrderDoesNotExist, SagaDoesNotExist
-from src.infrastructure.messaging.messages import OrderMessage
-from src.infrastructure.models import CreateOrderSagaStepModel, OrderModel
-from src.infrastructure.services.interfaces import IPaymentService
+from src.infrastructure.messaging.messages import ChargedPaymentMessage, OrderMessage
+from src.infrastructure.models import (
+    CreateOrderSagaModel,
+    CreateOrderSagaStepModel,
+    OrderModel,
+)
+from src.infrastructure.services.interfaces import INotificationService, IPaymentService
 from src.infrastructure.uow.interfaces import IUnitOfWork
 
 
@@ -11,26 +17,63 @@ class CreateOrderSaga:
         self,
         uow: IUnitOfWork,
         payment_service_proxy: IPaymentService,
+        notifications_service_proxy: INotificationService,
     ):
         self.uow = uow
-        self.payment = payment_service_proxy
+        self.payment_service = payment_service_proxy
+        self.notifications_service = notifications_service_proxy
 
-    async def on_order_created(self, message: OrderMessage):
+    async def on_charged_payment(self, message: ChargedPaymentMessage):
         async with self.uow:
-            await self.uow.create_order_saga.create(message)
-            await self.payment.verify(message.order_id)
+            saga: CreateOrderSagaModel = await self.uow.create_order_saga.find_one(
+                CreateOrderSagaModel.order_id, message.order_id
+            )
+
+            if not saga:
+                raise SagaDoesNotExist
+
+            if saga.state == CreateOrderSagaStatus.COMPLETED:
+                return
+
+            customer_id = saga.context['customer_id']
+            step = CreateOrderSagaStepModel(
+                saga_id=saga.id,
+                event_type=OrderEventTypes.ORDER_PAYED,
+                status=CreateOrderStepStatus.SUCCESS,
+                payload=message.model_dump(),
+            )
+            await self.uow.create_order_saga_step.add(step)
+
+            order = await self.uow.orders.load(message.order_id)
+            event = OrderPayed(order_id=message.order_id)
+            await self.uow.orders.append_events(
+                order_id=message.order_id,
+                expected_version=order.version,
+                events=[event],
+            )
+            order.apply(event)
+            await self.uow.orders.upsert_projection(order, customer_id)
+            await self.notifications_service.notify(
+                SendNotifyCommand(
+                    customer_id=saga.context['customer_id'],
+                    message=f'Заказ №{message.order_id} успешно оформлен',
+                )
+            )
+            saga.state = CreateOrderSagaStatus.COMPLETED
             await self.uow.commit()
 
     async def compensate(self, message: OrderMessage):
         async with self.uow:
-            saga = await self.uow.create_order_saga.get_by_order_id(message.order_id)
+            saga = await self.uow.create_order_saga.find_one(
+                CreateOrderSagaModel.order_id, message.order_id
+            )
 
             if not saga:
                 raise SagaDoesNotExist
 
             step = CreateOrderSagaStepModel(
                 saga_id=saga.id,
-                event_type=OrderStatus.FAILED,
+                event_type=OrderEventTypes.FAILED_CREATE_ORDER,
                 status=CreateOrderStepStatus.IN_PROGRESS,
                 payload=message.model_dump(),
             )
@@ -43,9 +86,7 @@ class CreateOrderSaga:
             if not order:
                 raise OrderDoesNotExist
 
-            await self.payment.cancel(order)
-
             step.status = CreateOrderStepStatus.COMPENSATED
-            saga.state = OrderStatus.FAILED
+            saga.state = CreateOrderSagaStatus.FAILED
 
             await self.uow.commit()
