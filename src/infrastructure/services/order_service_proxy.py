21c05a61fc5080f3282888774a762a19a39ef205
NormanwOw <normjkeee@vk.com>
2026-03-14 17:12:46 +0500

    ADDED order service proxy

---
+++ src/infrastructure/services/order_service_proxy.py
@@ -0,0 +1,21 @@
+from src.application.ports.services import IOrderServiceProxy
+from src.application.ports.uow import IUnitOfWork
+from src.config import Settings
+from src.domain.commands import CancelCommand
+from src.infrastructure.models import OutboxModel
+
+
+class OrderServiceProxy(IOrderServiceProxy):
+    def __init__(self, settings: Settings):
+        self.topic = settings.ORDER_COMMANDS_TOPIC
+        self.settings = settings
+
+    async def compensate(self, uow: IUnitOfWork, command: CancelCommand):
+        for_outbox = OutboxModel(
+            action=command.command_type,
+            topic=self.topic,
+            external_reference=command.external_reference.to_dict(),
+            producer=self.settings.SERVICE_NAME,
+            payload=command.payload,
+        )
+        await uow.outbox.add(for_outbox)
