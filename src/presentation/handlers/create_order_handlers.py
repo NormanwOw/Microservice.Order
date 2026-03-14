ed2cf74a2db82e5a0a16cc97eceeb8c9ff463e78
NormanwOw <normjkeee@vk.com>
2026-03-14 17:37:29 +0500

    UPDATED create_order_handlers, switched EventTypes to OrderEventTypes, added handle_cancel_order

--- src/presentation/handlers/create_order_handlers.py
+++ src/presentation/handlers/create_order_handlers.py
@@ -2,9 +2,11 @@ from src.application.disp_depends import DispDepends
 from src.application.dispatcher import dispatcher
 from src.application.ports.uow import IUnitOfWork
 from src.application.sagas.create_order_saga import CreateOrderSaga
-from src.domain.enums import EventTypes
+from src.application.use_cases.cancel_order import CancelOrder
+from src.domain.enums import CommandTypes, OrderEventTypes
 from src.infrastructure.logger.impl import logger
 from src.infrastructure.messaging.messages import (
+    CancelOrderMessage,
     FailedEventMessage,
     PaymentChargedMessage,
     ProductsCommittedMessage,
@@ -13,7 +15,7 @@ from src.infrastructure.messaging.messages import (
 from src.presentation.dependencies.order_dependencies import OrderDependencies
 
 
-@dispatcher.register(EventTypes.PRODUCTS_RESERVED)
+@dispatcher.register(OrderEventTypes.PRODUCTS_RESERVED)
 async def handle_products_reserved(
     uow: IUnitOfWork,
     message: dict,
@@ -24,7 +26,7 @@ async def handle_products_reserved(
     logger.info(f'Products reserved for order {msg.external_reference.id}')
 
 
-@dispatcher.register(EventTypes.PAYMENT_CHARGED)
+@dispatcher.register(OrderEventTypes.PAYMENT_CHARGED)
 async def handle_payment_charged(
     uow: IUnitOfWork,
     message: dict,
@@ -35,7 +37,7 @@ async def handle_payment_charged(
     logger.info(f'Payment charged for order {msg.external_reference.id}')
 
 
-@dispatcher.register(EventTypes.PRODUCTS_COMMITTED)
+@dispatcher.register(OrderEventTypes.PRODUCTS_COMMITTED)
 async def handle_products_committed(
     uow: IUnitOfWork,
     message: dict,
@@ -46,7 +48,7 @@ async def handle_products_committed(
     logger.info(f'Products committed for order {msg.external_reference.id}')
 
 
-@dispatcher.register(EventTypes.CHARGE_PAYMENT_FAILED)
+@dispatcher.register(OrderEventTypes.CHARGE_PAYMENT_FAILED)
 async def handle_charge_payment_failed(
     uow: IUnitOfWork,
     message: dict,
@@ -59,20 +61,7 @@ async def handle_charge_payment_failed(
     )
 
 
-@dispatcher.register(EventTypes.COMMIT_FAILED)
-async def handle_commit_products_failed(
-    uow: IUnitOfWork,
-    message: dict,
-    saga: CreateOrderSaga = DispDepends(OrderDependencies.create_order_saga),
-):
-    msg = FailedEventMessage(**message)
-    await saga.compensate(uow, msg)
-    logger.info(
-        f'Failed commit products. {msg.payload.error_message}. Order {msg.external_reference.id}'
-    )
-
-
-@dispatcher.register(EventTypes.RESERVE_FAILED)
+@dispatcher.register(OrderEventTypes.RESERVE_FAILED)
 async def handle_reserve_products_failed(
     uow: IUnitOfWork,
     message: dict,
@@ -83,3 +72,14 @@ async def handle_reserve_products_failed(
     logger.info(
         f'Failed reserve products. {msg.payload.error_message}. Order {msg.external_reference.id}'
     )
+
+
+@dispatcher.register(CommandTypes.CANCEL_ORDER)
+async def handle_cancel_order(
+    uow: IUnitOfWork,
+    message: dict,
+    cancel_order: CancelOrder = DispDepends(OrderDependencies.cancel_order),
+):
+    msg = CancelOrderMessage(**message)
+    await cancel_order(uow, msg)
+    logger.info(f'Cancelled order {msg.external_reference.id}')
